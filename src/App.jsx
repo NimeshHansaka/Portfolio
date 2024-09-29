cd847b856db95ea4966382d256594fbef6bb3a16
NimeshHansaka <nimeshhansaka1999@gmail.com>
2024-09-30 03:32:14 +0530

    Navbar Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
-import './App.css'
+import styles from './App.module.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
  
   return (
-    <>
-      Hello World
-    </>
+    <div className={styles.App}>
+      <Navbar/>
+    </div>
   )
 }
 
